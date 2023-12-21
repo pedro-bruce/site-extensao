313b3d1f5b032895032d96d25c9e70eb16c428e2
Pedro Bruce de Lima <98055885+pedro-bruce@users.noreply.github.com>
2023-12-21 10:43:45 -0300

    adicionando redirecionamento de botões

--- script.js
+++ script.js
@@ -9,3 +9,51 @@ const showMenu = (toggleId, navId) => {
 }
 
 showMenu('nav-toggle','nav-menu')
+
+function signUpRedirect() {
+    window.location.href = "cadastro.html";
+}
+
+function passwordRedirect() {
+    window.location.href = "fgtpassword.html";
+}
+
+function extRedirect() {
+    window.location.href = "extensao.html";
+}
+
+function projDispRedirect() {
+    window.location.href = "projetos-disponiveis.html";
+}
+
+function projAndRedirect() {
+    window.location.href = "projetos-em-andamento.html";
+}
+
+function projConclRedirect() {
+    window.location.href = "projetos-concluidos.html";
+}
+
+function newsRedirect() {
+    window.location.href = "noticias.html";
+}
+
+function projDispExampleRedirect() {
+    window.location.href = "proj-disp.html";
+}
+
+function projAndExampleRedirect() {
+    window.location.href = "proj-andamento.html";
+}
+
+function projAndExampleDonationRedirect() {
+    window.location.href = "proj-andamento-doacao.html";
+}
+
+function projConclExampleRedirect() {
+    window.location.href = "proj-concl.html";
+}
+
+function donationRedirect() {
+    window.location.href = "doacao.html"
+}
